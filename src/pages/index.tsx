52484b2491f14396baba1487603172f7af1b4b9c
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-01-18 00:13:06 -0300

    create slider in page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image";
 import { HomeContainer, Product } from "../styles/pages/home";
-
+import { useKeenSlider } from 'keen-slider/react' // biblioteca para criar o slider
 import camiseta1 from "../assets/camisetas/1.png"
 import camiseta2 from "../assets/camisetas/2.png"
 import camiseta3 from "../assets/camisetas/3.png"
+import 'keen-slider/keen-slider.min.css' //importando css da biblioteca do slider
 
 export default function Home() {
+  const [ sliderRef ] = useKeenSlider({ // refs são funcionalidades do react que nos permite ter acesso a uma referência de um elemento na dom -> esse hook retorna um array
+    slides: {
+      perView: 3, // para ficar aparecendo três produtos no slider
+      spacing: 48 // para colocar espaço entre os items do slider
+    }
+  })
+   
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider"> {/*passamos ref para o container que cerca o slider - precisamos passar essas classes para o slider funcionar*/}
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480} /> {/*quando usamos o Image do next é importante colocar altura e largura pra imagem não ficar com um tamanho muito grande*/}
         <footer> {/*melhor elemento pra colocar legenda na imagem*/}
           <strong>
@@ -19,7 +27,7 @@ export default function Home() {
           </span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} alt="" width={520} height={480} /> {/*quando usamos o Image do next é importante colocar altura e largura pra imagem não ficar com um tamanho muito grande*/}
         <footer> {/*melhor elemento pra colocar legenda na imagem*/}
           <strong>
@@ -30,8 +38,19 @@ export default function Home() {
           </span>
         </footer>
       </Product>
-      <Product>
-        <Image src={camiseta3} alt="" width={520} height={480} /> {/*quando usamos o Image do next é importante colocar altura e largura pra imagem não ficar com um tamanho muito grande*/}
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} alt="" width={520} height={480} /> {/*quando usamos o Image do next é importante colocar altura e largura pra imagem não ficar com um tamanho muito grande*/}
+        <footer> {/*melhor elemento pra colocar legenda na imagem*/}
+          <strong>
+            Camiseta X
+          </strong>
+          <span>
+            R$ 79,90
+          </span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} alt="" width={520} height={480} /> {/*quando usamos o Image do next é importante colocar altura e largura pra imagem não ficar com um tamanho muito grande*/}
         <footer> {/*melhor elemento pra colocar legenda na imagem*/}
           <strong>
             Camiseta X
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,7 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', { // vai ficar por volta de todo o carrossel
   display: 'flex', // para as camisetas ficarem um ao lado da outra
-  gap: '3rem',
+  //gap: '3rem', // essa propriedade não é calculada no slider
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2 ))', // vamos fazer esse calculo para quando diminuirmos o zoom o container sempre ficar centralizado - 1180px é o tamanho do container, vamos dividir por dois porque a subtração resulta no resto dos cantos da tela, a gente quer pegar apenas um dos cantos para subtrair com 100vw
   marginLeft: 'auto',
@@ -12,7 +12,7 @@ export const HomeContainer = styled('main', { // vai ficar por volta de todo o c
 export const Product = styled('a', { // aqui é onde vai ficar cada um dos produtos de dentro do carrossel
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
+  //padding: '0.25rem', // essa propriedade não é calculada no slider
   cursor: 'pointer',
   position: 'relative', 
   overflow: 'hidden', // para fazermos o efeito de hover sem criar a barra do lado e para o footer não surgir por cima da imagem e sim por dentro
