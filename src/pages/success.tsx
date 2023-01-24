322b7739789110292cd4732546d7825782e95891
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-01-24 00:08:52 -0300

    update page success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -8,12 +8,13 @@ import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 interface SuccessProps {
   customerName: string;
   product: {
-    name: string,
-    imageUrl: string,
-  }
+    name: string;
+    imageUrl: string;
+  };
 }
 
-export default function Success({ customerName, product }: SuccessProps) { // vamos pegar as informações da propriedade
+export default function Success({ customerName, product }: SuccessProps) {
+  // vamos pegar as informações da propriedade
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
@@ -22,8 +23,7 @@ export default function Success({ customerName, product }: SuccessProps) { // va
       </ImageContainer>
       <p>
         Uhuul <strong>{customerName}</strong>, sua{" "}
-        <strong>{product.name}</strong> já está a caminho da sua
-        casa.
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainer>
@@ -33,12 +33,20 @@ export default function Success({ customerName, product }: SuccessProps) { // va
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   // vamos importar a tipagem da função - essa função exige que seja retornado props
   // console.log nessa função (ou em getStaticProps) aparece no servidor node.js
+  if (!query.session_id) { // se não existir o session_id vai entrar nas chaves
+    return {
+      redirect: { 
+        destination: "/", 
+        permanent: false // não é sempre que o usuário será redirecionado, por isso 'false'
+      },
+    };
+  }
   const sessionId = String(query.session_id); // podemos pegar o session_id de dentro do query - vamos forçar para que seja uma String (porque session_id pode ser uma string ou array de string)
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
-    expand: ['line_items', 'line_items.data.price.product'] // aqui vamos expandir os dados do produto através do ID do relacionamento entre produto e dados desse produto e vamos expandir também os dados do produto relacionado ao preço para ter mais informações
-  }) //retrieve é quando queremos pegar um item (e não uma lista)
+    expand: ["line_items", "line_items.data.price.product"], // aqui vamos expandir os dados do produto através do ID do relacionamento entre produto e dados desse produto e vamos expandir também os dados do produto relacionado ao preço para ter mais informações
+  }); //retrieve é quando queremos pegar um item (e não uma lista)
   const customerName = session.customer_details.name; // pegando nome do cliente
-  const product = session.line_items.data[0].price.product as Stripe.Product // 'data' porque é um array, uma compra pode ter vários produtos (só podemos ter um - por isso o '0') - Stripe.Product na tipagem porque fizemos um expand no id do produto
+  const product = session.line_items.data[0].price.product as Stripe.Product; // 'data' porque é um array, uma compra pode ter vários produtos (só podemos ter um - por isso o '0') - Stripe.Product na tipagem porque fizemos um expand no id do produto
 
   return {
     props: {
@@ -46,7 +54,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
       product: {
         name: product.name,
         imageUrl: product.images[0],
-      }
+      },
     },
   };
 };
